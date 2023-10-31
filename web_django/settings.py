2924bdeb4bbcd8aa41198ef76b0971521c73e466
Sourcery AI <>
2023-10-31 06:46:34 +0000

    'Refactored by Sourcery'

--- web_django/settings.py
+++ web_django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,7 +129,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = '%s/appweb/static/' % BASE_DIR
+MEDIA_ROOT = f'{BASE_DIR}/appweb/static/'
 CKEDITOR_UPLOAD_PATH = 'uploads/lessons/contents/'
 
 REST_FRAMEWORK = {
